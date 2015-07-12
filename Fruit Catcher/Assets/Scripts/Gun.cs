929f5ee1485c86c3dbaf9b76731c7d1b56ba2b80
Wilson Censon <ken_nson@yahoo.com>
2015-07-12 09:22:24 +0800

    added initial commit

---
+++ Fruit Catcher/Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject claw;
+	public bool isShooting;
+	public Animator mineralAnimator;
+	public Claw clawScript;
+	
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && !isShooting) {
+			LaunchClaw();		
+		}
+	}
+
+	void LaunchClaw() {
+		isShooting = true;
+		mineralAnimator.speed = 0;
+		RaycastHit hit;
+		Vector3 down = transform.TransformDirection (Vector3.down);
+
+		if (Physics.Raycast (transform.position, down, out hit, 100)) {
+			claw.SetActive(true);
+			clawScript.ClawTarget(hit.point);
+		}
+	}
+
+	public void CollectedObject() {
+		isShooting = false;
+		mineralAnimator.speed = 1;
+	}
+}
---
+++ Fruit Catcher/Assets/Scripts/Spawns.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawns : MonoBehaviour {
+
+	public int xRange = 10;
+	public int yRange = 3;
+	public int numObjects = 16;
+
+	public GameObject[] objects;
+
+	void Start () {
+		Spawn ();
+	}
+	
+
+	void Spawn () {
+		for (int i = 0; i<= numObjects; i++) {
+			Vector3 spawnLoc = new Vector3 (Random.Range (-xRange, xRange), Random.Range (-yRange, yRange), 0);
+			int objectPick = Random.Range (0, objects.Length);
+			Instantiate (objects [objectPick], spawnLoc, Random.rotation);
+		}
+	}
+}
